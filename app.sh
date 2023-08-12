7d28f4fee405c25ef79fa8bb1b976c1c949a5c5d
chuhha <57899753+chuhha@users.noreply.github.com>
2023-08-12 22:37:11 +0300

    Create app.sh

---
+++ app.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+clear; sleep 5
+echo 'Установка необходимых программ начнется'
+echo 'сразу после проверочного обновления системы'
+sleep 3; clear
+echo '[|]';sleep 0.5; clear
+echo '[/]'; sleep 0.5; clear
+echo '[--]'; sleep 0.5; clear
+echo '[|]';sleep 0.5; clear
+echo '[\]'; sleep 0.5; clear
